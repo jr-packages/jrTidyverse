099f0b60408b4b770867748f1350066b3d73340c
theoroe3 <theo@jumpingrivers.com>
2018-02-06 17:05:32 +0000

    initial package framework

---
+++ R/data_help_files.R
@@ -0,0 +1,12 @@
+#' Example data frame data
+#'
+#' Used only for the introduction to data frames example
+#'
+#'@name example
+#'@docType data
+#'@usage data(example)
+#'@return A data frame
+#'@keywords datasets
+#'@examples
+#' data(example)
+NULL
