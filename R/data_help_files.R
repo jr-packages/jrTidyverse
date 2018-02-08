0ce0d7128e2d5de9e1900419e716781ff41e96e5
theoroe3 <theo@jumpingrivers.com>
2018-02-08 12:01:44 +0000

    adding sample join data

--- R/data_help_files.R
+++ R/data_help_files.R
@@ -10,3 +10,29 @@
 #'@examples
 #' data(example)
 NULL
+
+#' Stock prices example
+#'
+#' Used only for the dplyr joining example
+#'
+#'@name stock_price
+#'@docType data
+#'@usage data(stock_price)
+#'@return A data frame
+#'@keywords datasets
+#'@examples
+#' data(stock_price)
+NULL
+
+#' Stock price changes example
+#'
+#' Used only for the dplyr joining example
+#'
+#'@name stock_change
+#'@docType data
+#'@usage data(stock_change)
+#'@return A data frame
+#'@keywords datasets
+#'@examples
+#' data(stock_change)
+NULL
