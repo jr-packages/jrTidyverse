8843d8730b5dbc827b245849c3509f608c44ba44
Colin Gillespie <csgillespie@gmail.com>
2018-04-19 11:05:23 +0100

    Remove notes

---
+++ NEWS.md
@@ -0,0 +1,7 @@
+## 0.1.4
+  * Remove notes
+
+## 0.1.2
+
+  * Remove odd `x = 1` boxplot example
+  * Clarify `alpha` and points
--- R/get_notes.R
+++ R/get_notes.R
@@ -1,9 +1,9 @@
-#' View the notes for the jrTidyverse course
-#' 
-#' This function opens a browser to show the pdf of the handout notes. This way attendees can always get those notes back.
-#' 
-#' @importFrom utils browseURL
-#' @export
-get_notes = function(){
-  browseURL(system.file("notes.pdf",package = "jrTidyverse"))
-}
+#' #' View the notes for the jrTidyverse course
+#' #' 
+#' #' This function opens a browser to show the pdf of the handout notes. This way attendees can always get those notes back.
+#' #' 
+#' #' @importFrom utils browseURL
+#' #' @export
+#' get_notes = function(){
+#'   browseURL(system.file("notes.pdf",package = "jrTidyverse"))
+#' }
