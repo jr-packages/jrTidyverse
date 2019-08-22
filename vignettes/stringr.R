b00edc96cacf32aaf143dd9587fb2750baefba8a
Rhian Davies <rhian@jumpingrivers.com>
2019-08-22 12:55:10 +0100

    Rename vignettes

--- vignettes/stringr.R
+++
@@ -1,8 +0,0 @@
-## ----setup, include=FALSE------------------------------------------------
-knitr::opts_chunk$set(echo = FALSE, cache= TRUE, results = "hide", fig.align = "center", fig.keep='none')
-
-## ------------------------------------------------------------------------
-movies %>% 
-  ggplot(aes(x = title_length, y = rating)) + 
-  geom_point()
-
